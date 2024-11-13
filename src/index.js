8d733d557ca9da2d6827b12df9ee0fafdd866266
Ganesh Patil <patil7227ganesh@gmail.com>
2024-11-13 22:53:52 +0530

    utils added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import { databaseConnection } from "./db/index.js";
 
 databaseConnection()
+.then(
+    app.listen(process.env.PORT || 8000 , () => {
+        console.log(`Server is listening on port ${process.env.PORT || 8000}`);
+    })
+)
+.catch((error) => {
+    console.log("Database connection error");
+})
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,23 @@
+const asyncHandler = (requestHandler) => {
+    (req,res,next) => {
+        Promise.resolve(requestHandler(req,res,next))
+        .catch((error) => next(error))
+    }
+}
+
+
+export {asyncHandler}
+
+
+
+// wrapper function ..............................................
+//  const asyncHandler = (requestHandler) => async (req,res,next) =>{
+//     try{
+//         requestHandler(req,res,next);
+//     }catch(error){
+//         res.status(error.code || 500).json({
+//             success : false,
+//             message : error.message
+//         })
+//     }
+//  }
