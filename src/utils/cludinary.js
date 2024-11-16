fe463ad3903dbd2e112e466bdb944ce97d766b29
Ganesh Patil <patil7227ganesh@gmail.com>
2024-11-16 20:38:18 +0530

    multer setup complete

--- src/utils/cludinary.js
+++ src/utils/cludinary.js
@@ -6,3 +6,19 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET, // Click 'View API Keys' above to copy your API secret
 });
+
+const uploadOnCloudinary = async (localeFilePath) => {
+  try {
+    if(!localeFilePath) return null // localepath not valid then return with null
+    const response = await cloudinary.uploader.upload(localeFilePath , {          // upload the file to clodinary
+      resource_type : "auto"
+    })
+    console.log(`File has been uploaded successfully ${response.url}`)
+    return response
+  } catch (error) {
+    fs.unlinkSync(localeFilePath); // remove file from loacalefile system when not uploaded for cleaning purpose
+    return null
+  }
+}
+
+export {uploadOnCloudinary}
