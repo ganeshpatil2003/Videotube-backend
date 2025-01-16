c5dca64f6f65671ccfcd6200b9a671699ea6c439
Ganesh Patil <patil7227ganesh@gmail.com>
2025-01-17 00:31:16 +0530

    refactor app.js for improved code consistency and formatting

--- src/app.js
+++ src/app.js
@@ -10,37 +10,42 @@ import { commentRoutes } from "./routes/comment.routes.js";
 import { likeRoutes } from "./routes/like.routes.js";
 const app = express();
 
-app.use(cors({
-    origin : process.env.CORS_ORIGIN,
-    credentials : true
-}))
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  }),
+);
 
-app.use(express.json({
-    limit : "16kb"
-}))
+app.use(
+  express.json({
+    limit: "16kb",
+  }),
+);
 
-app.use(express.urlencoded({
-    extended : true,
-    limit : "16kb"
-}))
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "16kb",
+  }),
+);
 
-app.use(express.static("public"))
+app.use(express.static("public"));
 
-app.use(cookieParser())
+app.use(cookieParser());
 
-app.use("/api/v1/users",userRouter)
+app.use("/api/v1/users", userRouter);
 
-app.use('/api1/v1/videoes',videoRoutes);
+app.use("/api1/v1/videoes", videoRoutes);
 
-app.use('/api1/v1/tweets',tweetRoutes);
+app.use("/api1/v1/tweets", tweetRoutes);
 
-app.use('/api1/v1/playLists',playListRoutes);
+app.use("/api1/v1/playLists", playListRoutes);
 
-app.use('/api1/v1/subscriptions',subscriptionRoutes);
+app.use("/api1/v1/subscriptions", subscriptionRoutes);
 
-app.use('/api1/v1/comments',commentRoutes);
+app.use("/api1/v1/comments", commentRoutes);
 
-app.use('/api1/v1/likes',likeRoutes);
-
+app.use("/api1/v1/likes", likeRoutes);
 
 export { app };
